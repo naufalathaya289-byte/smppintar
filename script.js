f9d24a551fb9430526e86c32106148695c7860c9
naufalathaya289-byte <naufalathaya289@gmail.com>
2026-06-05 15:27:41 +0700

    Add files via upload
    Initial website upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// alert selamat datang
+window.onload = function() {
+    console.log("Website SMP Pintar siap digunakan");
+    // bisa juga tambahkan pesan sambutan
+    // alert("Selamat datang di website resmi SMP Pintar");
+}
